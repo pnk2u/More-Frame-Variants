74770e2ec7e958e43503a4e673caf4941a409ba8
pnku <159317511+pnk2u@users.noreply.github.com>
2025-08-14 23:34:02 +0200

    1.2.0 clean-up + fix gl.i-f map model

--- src/client/java/de/pnku/mstv_mframev/compat/fastitemframes/MoreFrameVariantsCompatibilityFIFClientConstructor.java
+++ src/client/java/de/pnku/mstv_mframev/compat/fastitemframes/MoreFrameVariantsCompatibilityFIFClientConstructor.java
@@ -1,23 +1,14 @@
 package de.pnku.mstv_mframev.compat.fastitemframes;
 
 import de.pnku.mstv_mframev.item.MoreFrameVariantItems;
-import fuzs.fastitemframes.init.ModRegistry;
-import fuzs.fastitemframes.world.level.block.entity.ItemFrameBlockEntity;
 import fuzs.puzzleslib.api.client.core.v1.ClientModConstructor;
 import fuzs.puzzleslib.api.client.core.v1.context.ColorProvidersContext;
 import fuzs.puzzleslib.api.client.core.v1.context.ItemModelPropertiesContext;
-import net.minecraft.client.color.block.BlockColor;
 import net.minecraft.client.color.item.ItemColor;
-import net.minecraft.core.BlockPos;
 import net.minecraft.core.component.DataComponents;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.component.DyedItemColor;
-import net.minecraft.world.level.BlockAndTintGetter;
 import net.minecraft.world.level.ItemLike;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.entity.BlockEntity;
-import net.minecraft.world.level.block.state.BlockState;
-import org.jetbrains.annotations.Nullable;
 
 import static fuzs.fastitemframes.client.FastItemFramesClient.DYED_MODEL_PROPERTY;
 
@@ -26,21 +17,7 @@ public class MoreFrameVariantsCompatibilityFIFClientConstructor implements Clien
         context.registerItemProperty(DYED_MODEL_PROPERTY, (itemStack, clientLevel, livingEntity, i) -> itemStack.has(DataComponents.DYED_COLOR) ? 1.0F : 0.0F, MoreFrameVariantItems.more_all_item_frames.toArray(new ItemLike[0]));
     }
 
-    public void onRegisterBlockColorProviders(ColorProvidersContext<Block, BlockColor> context) {
-        context.registerColorProvider((BlockColor)(BlockState blockState, @Nullable BlockAndTintGetter blockAndTintGetter, @Nullable BlockPos blockPos, int tintIndex) -> {
-            if (blockAndTintGetter != null && blockPos != null) {
-                BlockEntity patt0$temp = blockAndTintGetter.getBlockEntity(blockPos);
-                if (patt0$temp instanceof ItemFrameBlockEntity) {
-                    ItemFrameBlockEntity blockEntity = (ItemFrameBlockEntity)patt0$temp;
-                    return blockEntity.getColor().orElse(-1);
-                }
-            }
-
-            return -6265536;
-        }, new Block[]{(Block) ModRegistry.ITEM_FRAME_BLOCK.value(), (Block)ModRegistry.GLOW_ITEM_FRAME_BLOCK.value()});
-    }
-
     public void onRegisterItemColorProviders(ColorProvidersContext<Item, ItemColor> context) {
-        context.registerColorProvider((ItemColor) (itemStack, tintIndex) -> tintIndex == 0 ? DyedItemColor.getOrDefault(itemStack, -1) : -1, MoreFrameVariantItems.more_all_item_frames.toArray(new Item[0]));
+        context.registerColorProvider((itemStack, tintIndex) -> tintIndex == 0 ? DyedItemColor.getOrDefault(itemStack, -1) : -1, MoreFrameVariantItems.more_all_item_frames.toArray(new Item[0]));
     }
 }
--- src/main/java/de/pnku/mstv_mframev/compat/fastitemframes/mixin/ItemFrameHandlerMixin.java
+++ src/main/java/de/pnku/mstv_mframev/compat/fastitemframes/mixin/ItemFrameHandlerMixin.java
@@ -14,7 +14,6 @@ import net.minecraft.world.entity.decoration.ItemFrame;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
-import org.spongepowered.asm.mixin.Debug;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -22,7 +21,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import static de.pnku.mstv_mframev.compat.fastitemframes.MoreFrameVariantsCompatibilityFIF.*;
 
-@Debug(export = true)
 @Mixin(ItemFrameHandler.class)
 public class ItemFrameHandlerMixin {
 
@@ -30,7 +28,7 @@ public class ItemFrameHandlerMixin {
     private static void injectedOnEntityLoad(Entity entity, ServerLevel serverLevel, CallbackInfoReturnable<EventResult> cir) {
         if (entity.getType().is(ModRegistry.ITEM_FRAMES_ENTITY_TYPE_TAG) && entity instanceof ItemFrame itemFrame) {
             serverLevel.getServer().tell(new TickTask(serverLevel.getServer().getTickCount(), () -> {
-                Block block = (Block) ItemFrameBlock.BY_ITEM.get(itemFrame.getFrameItemStack().getItem());
+                Block block = ItemFrameBlock.BY_ITEM.get(itemFrame.getFrameItemStack().getItem());
                 BlockPos blockPos = entity.blockPosition();
                 if (block != null && serverLevel.hasChunkAt(blockPos) && serverLevel.isEmptyBlock(blockPos)) {
                     serverLevel.setBlock(blockPos, (BlockState)block.defaultBlockState().setValue(ItemFrameBlock.FACING, itemFrame.getDirection()).setValue(WOOD_TYPE, WoodType.getEnumByName(((IItemFrame) itemFrame).mframev$getIFWoodVariant())), 2);
